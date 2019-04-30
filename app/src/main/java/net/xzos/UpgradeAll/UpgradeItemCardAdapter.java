31a30aabdc3c899da68252de61039f00484a0331
xz-dev <xiangzhedev@gmail.com>
2019-04-30 20:10:46 +0800

    Refactor to androidx

--- app/src/main/java/net/xzos/UpgradeAll/UpgradeItemCardAdapter.java
+++ app/src/main/java/net/xzos/UpgradeAll/UpgradeItemCardAdapter.java
@@ -2,9 +2,9 @@ package net.xzos.UpgradeAll;
 
 import android.content.Context;
 import android.content.Intent;
-import android.support.annotation.NonNull;
-import android.support.v7.widget.CardView;
-import android.support.v7.widget.RecyclerView;
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
--- app/src/main/java/net/xzos/UpgradeAll/UpgradeItemSettingActivity.java
+++ app/src/main/java/net/xzos/UpgradeAll/UpgradeItemSettingActivity.java
@@ -2,7 +2,7 @@ package net.xzos.UpgradeAll;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
