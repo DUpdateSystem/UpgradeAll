e9300a47d85139dbda79c691da6f21a52b941453
xz-dev <xiangzhedev@gmail.com>
2019-11-08 17:32:59 +0800

    UI 修复
    BugFix: 修复等待占位符不显示的问题；修复从跟踪项详情页到编辑页面悬浮按钮不显示的问题

--- app/src/main/kotlin/net/xzos/upgradeAll/ui/viewmodels/fragment/AppListPlaceholderFragment.kt
+++ app/src/main/kotlin/net/xzos/upgradeAll/ui/viewmodels/fragment/AppListPlaceholderFragment.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
@@ -32,7 +33,9 @@ class AppListPlaceholderFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? =
-            inflater.inflate(R.layout.pageview_app_list, container, false)
+            inflater.inflate(R.layout.pageview_app_list, container, false).apply {
+                this.findViewById<LinearLayout>(R.id.placeholderLayout).visibility = View.VISIBLE
+            }
 
     @SuppressLint("SetTextI18n")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
