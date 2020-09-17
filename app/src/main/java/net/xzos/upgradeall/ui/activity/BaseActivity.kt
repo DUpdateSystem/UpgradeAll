13430fecd0605ccc25d1d72851115db5d6759187
xz-dev <xiangzhedev@gmail.com>
2020-09-17 21:41:23 +0800

    ci: 发布内测版本

--- app/src/main/java/net/xzos/upgradeall/ui/activity/BaseActivity.kt
+++ app/src/main/java/net/xzos/upgradeall/ui/activity/BaseActivity.kt
@@ -1,15 +1,14 @@
 package net.xzos.upgradeall.ui.activity
 
-import android.os.Bundle
 import com.akexorcist.localizationactivity.ui.LocalizationActivity
 import net.xzos.upgradeall.data.PreferencesMap
 
 open class BaseActivity : LocalizationActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onResume() {
         with(PreferencesMap.language_locale_code) {
             if (getCurrentLanguage() != this)
                 setLanguage(this)
         }
-        super.onCreate(savedInstanceState)
+        super.onResume()
     }
 }
