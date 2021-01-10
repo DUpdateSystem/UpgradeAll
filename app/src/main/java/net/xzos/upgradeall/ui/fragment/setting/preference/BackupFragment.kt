3f870788100d3a16f973c30e7b0f9b265f6734e7
xz-dev <xiangzhedev@gmail.com>
2021-01-10 17:05:23 +0800

    feat: 关联备份功能

--- app/src/main/java/net/xzos/upgradeall/ui/fragment/setting/preference/BackupFragment.kt
+++ app/src/main/java/net/xzos/upgradeall/ui/fragment/setting/preference/BackupFragment.kt
@@ -3,6 +3,7 @@ package net.xzos.upgradeall.ui.fragment.setting.preference
 import android.os.Bundle
 import android.text.InputType
 import android.view.View
+import android.widget.Toast
 import androidx.preference.EditTextPreference
 import androidx.preference.Preference
 import kotlinx.coroutines.Dispatchers
@@ -77,7 +78,11 @@ class BackupFragment : PrefFragment(R.xml.preferences_backup) {
         val backupPreference: Preference = findPreference("WEBDAV_BACKUP")!!
         val restorePreference: Preference = findPreference("WEBDAV_RESTORE")!!
         backupPreference.setOnPreferenceClickListener {
-            CloudBackupManager().backup()
+            CloudBackupManager().backup(
+                    { MiscellaneousUtils.showToast(R.string.backup_running) },
+                    { MiscellaneousUtils.showToast(R.string.backup_stop) },
+                    { MiscellaneousUtils.showToast(it.message.toString(), Toast.LENGTH_LONG) },
+            )
             false
         }
         restorePreference.setOnPreferenceClickListener {
@@ -88,7 +93,12 @@ class BackupFragment : PrefFragment(R.xml.preferences_backup) {
                     withContext(Dispatchers.Main) {
                         CloudBackupListDialog.show(it, fileNameList, fun(position) {
                             GlobalScope.launch {
-                                cloudBackupManager.restoreBackup(fileNameList[position])
+                                cloudBackupManager.restoreBackup(
+                                        fileNameList[position],
+                                        { MiscellaneousUtils.showToast(R.string.restore_running) },
+                                        { MiscellaneousUtils.showToast(R.string.restore_stop) },
+                                        { MiscellaneousUtils.showToast(it.message.toString(), Toast.LENGTH_LONG) },
+                                )
                             }
                         })
                     }
