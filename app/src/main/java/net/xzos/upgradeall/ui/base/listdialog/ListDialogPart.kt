955b4cdf82661bb7933f4f885aa4e9b04cf43a5a
Bozhen Zhao <zhaobozhen2025@gmail.com>
2021-03-05 20:58:40 +0800

    perf: 优化选择下载 dialog 布局。

--- app/src/main/java/net/xzos/upgradeall/ui/base/listdialog/ListDialogPart.kt
+++ app/src/main/java/net/xzos/upgradeall/ui/base/listdialog/ListDialogPart.kt
@@ -1,6 +1,7 @@
 package net.xzos.upgradeall.ui.base.listdialog
 
 import android.view.View
+import androidx.recyclerview.widget.RecyclerView
 import net.xzos.upgradeall.databinding.RecyclerlistContentBinding
 
 interface ListDialogPart {
@@ -10,6 +11,7 @@ interface ListDialogPart {
         contentBinding.apply {
             if (adapter.itemCount > 0) {
                 rvList.adapter = adapter
+                rvList.overScrollMode = RecyclerView.OVER_SCROLL_NEVER
             } else {
                 vfContainer.displayedChild = 1
                 tvEmpty.visibility = View.VISIBLE
