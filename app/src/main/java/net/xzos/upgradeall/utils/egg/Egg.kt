643f6cf49a25ed4603fd96c08d9ad5cfe1e93c9a
xz-dev <xiangzhedev@gmail.com>
2021-07-29 09:35:13 +0800

    fix: 修复彩蛋模块无意义的报错

--- app/src/main/java/net/xzos/upgradeall/utils/egg/Egg.kt
+++ app/src/main/java/net/xzos/upgradeall/utils/egg/Egg.kt
@@ -11,6 +11,7 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.runBlocking
 import net.xzos.upgradeall.R
 import net.xzos.upgradeall.application.MyApplication.Companion.context
+import net.xzos.upgradeall.core.data.backup.getOrNull
 import net.xzos.upgradeall.core.log.Log
 import net.xzos.upgradeall.core.log.ObjectTag
 import net.xzos.upgradeall.core.module.network.OkHttpApi
@@ -43,6 +44,7 @@ fun getEggDayOnline(): Day? {
     val holidayName: String
     try {
         val json = JSONObject(OkHttpApi.get(logObjectTag, url)?.body?.string() ?: return null)
+        json.getOrNull("holiday") ?: return null
         val holidayJson = json.getJSONObject("holiday")
         holidayName = holidayJson.getString("name")
     } catch (e: JSONException) {
