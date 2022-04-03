ef264f1a7623b1cbfbee20dbd857548d440163ed
xz-dev <xiangzhedev@gmail.com>
2022-04-03 08:57:23 +0800

    fix(downloader): Fix crash

--- core-downloader/src/main/java/net/xzos/upgradeall/core/downloader/filedownloader/item/TaskWrapper.kt
+++ core-downloader/src/main/java/net/xzos/upgradeall/core/downloader/filedownloader/item/TaskWrapper.kt
@@ -1,5 +1,7 @@
 package net.xzos.upgradeall.core.downloader.filedownloader.item
 
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.runBlocking
 import net.xzos.upgradeall.core.utils.coroutines.coroutinesMutableListOf
 import zlc.season.rxdownload4.manager.*
 
@@ -11,27 +13,32 @@ class TaskWrapper internal constructor(internal val manager: TaskManager) {
     private val subscribeList = coroutinesMutableListOf<(TaskSnap) -> Unit>()
 
     init {
-        manager.subscribe { status ->
-            val downloadStatus = when (status) {
-                is Normal -> Status.START
-                is Pending -> Status.START
-                is Started -> Status.START
-                is Downloading -> Status.RUNNING
-                is Paused -> Status.STOP
-                is Completed -> Status.COMPLETE
-                is Failed -> Status.FAIL
-                is Deleted -> Status.CANCEL
+        runBlocking(Dispatchers.Main) {
+            manager.subscribe { status ->
+                subscribeFun(status)
             }
-            val progress = status.progress
-            manager.currentStatus()
-            val oldSnap = this.snap
-            val snap = TaskSnap(downloadStatus, progress.downloadSize, progress.totalSize)
-            oldSnap?.also { snap.countSpeed(it) }
-            this.snap = snap
-            subscribeList.map { it(snap) }
         }
     }
 
+    private fun subscribeFun(status: zlc.season.rxdownload4.manager.Status) {
+        val downloadStatus = when (status) {
+            is Normal -> Status.START
+            is Pending -> Status.START
+            is Started -> Status.START
+            is Downloading -> Status.RUNNING
+            is Paused -> Status.STOP
+            is Completed -> Status.COMPLETE
+            is Failed -> Status.FAIL
+            is Deleted -> Status.CANCEL
+        }
+        val progress = status.progress
+        manager.currentStatus()
+        val oldSnap = this@TaskWrapper.snap
+        val snap = TaskSnap(downloadStatus, progress.downloadSize, progress.totalSize)
+        oldSnap?.also { snap.countSpeed(it) }
+        this@TaskWrapper.snap = snap
+        subscribeList.map { it(snap) }
+    }
 
     internal fun subscribe(function: (TaskSnap) -> Unit) {
         subscribeList.add(function)
