728a534edf700755109e81ed85e20703119eb617
xz-dev <xiangzhedev@gmail.com>
2022-04-04 00:18:24 +0800

    chore(ui): remove debug print

--- app/src/main/java/net/xzos/upgradeall/ui/base/recycleview/RecyclerViewAdapter.kt
+++ app/src/main/java/net/xzos/upgradeall/ui/base/recycleview/RecyclerViewAdapter.kt
@@ -46,7 +46,6 @@ abstract class RecyclerViewAdapter<LT, L : ListItemView, RHA : RecyclerViewHandl
     abstract fun getViewHolder(layoutInflater: LayoutInflater, viewGroup: ViewGroup): T
 
     override fun onBindViewHolder(viewHolder: T, position: Int) {
-        println(position)
         val itemView = listContainerViewConvertFun(dataSet[position])
         viewHolder.bind(itemView)
         lifecycleScope.launch {
