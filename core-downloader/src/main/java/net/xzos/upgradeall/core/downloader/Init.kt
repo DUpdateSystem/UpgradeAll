a84ef107e833d0c5bfadff1870d7a72f998714e8
xz-dev <xiangzhedev@gmail.com>
2022-07-02 22:00:25 +0800

    fix(core-downloader/RxDownloader): patch RxJava for unhandled error

--- core-downloader/src/main/java/net/xzos/upgradeall/core/downloader/Init.kt
+++ core-downloader/src/main/java/net/xzos/upgradeall/core/downloader/Init.kt
@@ -1,6 +1,10 @@
 package net.xzos.upgradeall.core.downloader
 
 import android.annotation.SuppressLint
+import io.reactivex.plugins.RxJavaPlugins
+import net.xzos.upgradeall.core.utils.log.Log
+import net.xzos.upgradeall.core.utils.log.ObjectTag
+import net.xzos.upgradeall.core.utils.log.ObjectTag.Companion.core
 
 @SuppressLint("StaticFieldLeak")
 lateinit var downloadConfig: DownloadConfig
@@ -8,5 +12,17 @@ lateinit var downloadConfig: DownloadConfig
 fun initDownload(
     _downloadConfig: DownloadConfig,
 ) {
+    patchRxJava()
     downloadConfig = _downloadConfig
+}
+
+// fix rxjava
+private fun patchRxJava() {
+    if (RxJavaPlugins.getErrorHandler() == null) {
+        RxJavaPlugins.setErrorHandler {
+            val tag = "RxJava"
+            val logObjectTag = ObjectTag(core, tag)
+            Log.e(logObjectTag, tag, it.stackTraceToString())
+        }
+    }
 }
